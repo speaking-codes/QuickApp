389da558b8403b9de1434afc6d4187a3ef7569f4
mauro.diliddo <mauro.diliddo@FGBAL031929>
2023-12-24 12:20:28 +0100

    Customer Grid prima versione

--- QuickApp/ClientApp/src/app/components/customers/customers.component.ts
+++ QuickApp/ClientApp/src/app/components/customers/customers.component.ts
@@ -4,8 +4,22 @@
 // (c) 2023 www.ebenmonney.com/mit-license
 // ---------------------------------------
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { TableColumn } from '@swimlane/ngx-datatable';
+import { SearchBoxComponent } from '../controls/search-box.component';
+
+import { Customer } from 'src/app/models/customer';
+import { Role } from 'src/app/models/role.model';
+
 import { fadeInOut } from '../../services/animations';
+import { CustomerService } from 'src/app/services/customer.service';
+import { AlertService, DialogType, MessageSeverity } from 'src/app/services/alert.service';
+import { Utilities } from 'src/app/services/utilities';
+
+interface CustomerIndex extends Customer{
+  index: number;
+}
 
 @Component({
   selector: 'app-customers',
@@ -13,6 +27,66 @@ import { fadeInOut } from '../../services/animations';
   styleUrls: ['./customers.component.scss'],
   animations: [fadeInOut]
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+  columns: TableColumn[] = [];
+  rows: Customer[] = [];
+  rowsCache: Customer[] = [];
+  loadingIndicator = false;  
+  
+  constructor(private customerService: CustomerService, private alertService: AlertService){}
+  
+  ngOnInit(): void {
+    this.columns = [
+      { prop: 'index', name: '#', width: 40, canAutoResize: false, sortable: false },
+      { prop: 'fullName', name: 'Nominativo', canAutoResize: false, sortable: true },
+      { prop: 'birthDate', name: 'Data di Nascita'},
+      { prop: 'taxIdCode', name: 'Codice Fiscale', canAutoResize: false, sortable: true },
+      { prop: 'gender', name: 'Sesso', canAutoResize: false, sortable: false},
+      { prop: 'city', name: 'Residenza', canAutoResize: false, sortable: false},
+      { prop: 'details', name: '', canAutoResize: false, sortable: false}
+    ];
 
+    this.columns.push({
+      name: '',
+      width: 160,
+      // cellTemplate: this.actionsTemplate,
+      resizeable: false,
+      canAutoResize: false,
+      sortable: false,
+      draggable: false
+    });
+
+    this.loadData();
+  }
+
+  loadData(): void{
+    this.loadingIndicator = true;
+ 
+    this.customerService.getCustomers()
+        .subscribe({
+          next: results => this.onDataLoadSuccessful(results),
+          error: error => this.onDataLoadFailed(error)
+        });
+  }
+
+  onDataLoadSuccessful(customers: Customer[]) {
+    this.alertService.stopLoadingMessage();
+    this.loadingIndicator = false;
+
+    customers.forEach((customer, index) => {
+      (customer as CustomerIndex).index = index + 1;
+    });
+
+    this.rowsCache = [...customers];
+    this.rows = customers;
+  }
+  
+  onDataLoadFailed(error: HttpErrorResponse) {
+    this.alertService.stopLoadingMessage();
+    this.loadingIndicator = false;
+
+    this.alertService.showStickyMessage('Load Error',
+      `Unable to retrieve users from the server.\r\nError: "${Utilities.getHttpResponseMessage(error)}"`,
+      MessageSeverity.error, error);
+  }
 }
